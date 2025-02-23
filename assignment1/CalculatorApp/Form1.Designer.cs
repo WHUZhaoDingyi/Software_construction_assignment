5582a3349c2752e895ed59f3e3dca8115bf1f7cf
WHUZhaoDingyi <1369829994@qq.com>
2025-02-23 18:27:26 +0800

    第一次作业

---
+++ assignment1/CalculatorApp/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CalculatorApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private TextBox txtNum1;
+        private TextBox txtNum2;
+        private Button btnAdd;
+        private Button btnSubtract;
+        private Button btnMultiply;
+        private Button btnDivide;
+        private Label lblResult;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+
+            this.SuspendLayout();
+
+            // txtNum1
+            this.txtNum1.Location = new System.Drawing.Point(12, 12);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 22);
+            this.txtNum1.TabIndex = 0;
+
+            // txtNum2
+            this.txtNum2.Location = new System.Drawing.Point(12, 40);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 22);
+            this.txtNum2.TabIndex = 1;
+
+            // btnAdd
+            this.btnAdd.Location = new System.Drawing.Point(12, 68);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "加法";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+
+            // btnSubtract
+            this.btnSubtract.Location = new System.Drawing.Point(93, 68);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(75, 23);
+            this.btnSubtract.TabIndex = 3;
+            this.btnSubtract.Text = "减法";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+
+            // btnMultiply
+            this.btnMultiply.Location = new System.Drawing.Point(174, 68);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiply.TabIndex = 4;
+            this.btnMultiply.Text = "乘法";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+
+            // btnDivide
+            this.btnDivide.Location = new System.Drawing.Point(255, 68);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 23);
+            this.btnDivide.TabIndex = 5;
+            this.btnDivide.Text = "除法";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+
+            // lblResult
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 94);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(37, 17);
+            this.lblResult.TabIndex = 6;
+            this.lblResult.Text = "结果:";
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(342, 126);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "Form1";
+            this.Text = "计算器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ assignment1/CalculatorConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 提示用户输入第一个数字
+        Console.WriteLine("Enter the first number:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        // 提示用户输入第二个数字
+        Console.WriteLine("Enter the second number:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        // 提示用户输入运算符
+        Console.WriteLine("Enter an operator (+, -, *, /):");
+        string operatorInput = Console.ReadLine();
+
+        // 初始化计算结果变量
+        double result = 0;
+
+        // 根据运算符进行运算
+        switch (operatorInput)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Cannot divide by zero.");
+                    return; // 退出程序
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid operator. Please enter one of the following: +, -, *, /.");
+                return; // 退出程序
+        }
+
+        // 输出计算结果
+        Console.WriteLine($"The result of {num1} {operatorInput} {num2} = {result}");
+    }
+}
+
