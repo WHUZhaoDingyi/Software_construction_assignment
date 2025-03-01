b0d1cf7e7d9f20ac65b5cb109f97bb23c8c0288f
WHUZhaoDingyi <1369829994@qq.com>
2025-03-01 17:54:19 +0800

    2

---
+++ assignment2/SieveOfEratosthenes/Sieve.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const int limit=100;
+        bool[] isPrime=new bool[limit+1];
+
+        for (int i =2;i<=limit;i++)
+        {
+            isPrime[i]=true;
+
+            for(int i=2;i*i<=limit;i++)
+            {
+                if(isPrime[i])
+                {
+                    for (int j=i*i;j<=limit;j+=i)
+                        isPrime[j]=false;
+                }
+            }
+        }
+        Console.WriteLine("2~100以内的素数： ");
+            for(int i=2;i<=limit;i++)
+            {
+                if(isPrime[i])
+                    Console.Write(i+" ");
+            }
+    }
+}
--- assignment2/SieveOfEratosthenes/obj/Debug/net9.0/SieveOfEratosthenes.AssemblyInfo.cs
+++ assignment2/SieveOfEratosthenes/obj/Debug/net9.0/SieveOfEratosthenes.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ToeplitzChecker")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SieveOfEratosthenes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7872b20b94be97764d847652891de09ef1a8527f")]
-[assembly: System.Reflection.AssemblyProductAttribute("ToeplitzChecker")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ToeplitzChecker")]
+[assembly: System.Reflection.AssemblyProductAttribute("SieveOfEratosthenes")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SieveOfEratosthenes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
